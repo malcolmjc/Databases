af5b6f81ab05d1f7773f4355d9081e1d6917d863
Malcolm <mjcraney@calpoly.edu>
2019-10-12 21:33:26 -0700

    Fix Setup

--- lab2/CUSTOM/CUSTOM-setup.sql
+++ lab2/CUSTOM/CUSTOM-setup.sql
@@ -1,21 +1,21 @@
-DROP TABLE IF EXISTS Weather;
 DROP TABLE IF EXISTS Climbs;
+DROP TABLE IF EXISTS Weather;
 
 CREATE TABLE Weather (
    Date DATE PRIMARY KEY,
-   BatteryVoltageAVG DECIMAL(35, 30),
-   TemperatureAVG DECIMAL(35, 30),
-   RelativeHumidityAVG DECIMAL(35, 30),
-   WindSpeedDailyAVG DECIMAL(35, 30),
-   WindDirectionAVG DECIMAL(35, 30),
-   SolareRadiationAVG DECIMAL(35, 30)
+   BatteryVoltageAVG DECIMAL(20, 10),
+   TemperatureAVG DECIMAL(20, 10),
+   RelativeHumidityAVG DECIMAL(20, 10),
+   WindSpeedDailyAVG DECIMAL(20, 10),
+   WindDirectionAVG DECIMAL(20, 10),
+   SolareRadiationAVG DECIMAL(20, 10)
 );
 
 CREATE TABLE Climbs (
    Id INT PRIMARY KEY,
    Date DATE,
+   Route VARCHAR(255),
    Attempted INT,
    Succeeded INT,
-   SuccessPercentage DECIMAL(20,19)
-   FOREIGN KEY (Date) REFERENCES Weather (Date)
+   SuccessPercentage DECIMAL(15,13)
 );
