d8dcc7a303a29dafb706021861da9846aafa9104
Malcolm Craney <mjcraney@calpoly.edu>
2019-11-27 14:45:31 -0800

    R1

--- lab7/src/QueryBuilder.java
+++ lab7/src/QueryBuilder.java
@@ -1,4 +1,4 @@
-import java.sql.Connection;
+import java.sql.*;
 
 public class QueryBuilder {
    private Connection connection;
@@ -8,8 +8,62 @@ public class QueryBuilder {
    }
 
    public String roomsAndRates() {
-      // TODO: build and execute statement, return string result
-      return "";
+      String sql =
+            "with DaysOccupiedLast180 as (\n" +
+            "    select\n" +
+            "    Room,\n" +
+            "    SUM(DateDiff(Checkout,\n" +
+            "    case\n" +
+            "        when CheckIn >=  Current_Date - interval 180 day\n" +
+            "        then CheckIn\n" +
+            "        else Current_Date - interval 180 day\n" +
+            "    end\n" +
+            "    )) as DaysOccupied\n" +
+            "    from lab7_reservations\n" +
+            "    join lab7_rooms on Room = RoomCode\n" +
+            "    where CheckOut > Current_Date - interval 180 day\n" +
+            "    group by Room\n" +
+            "),\n" +
+            "MostRecentReservation as (\n" +
+            "    select Room,\n" +
+            "    MAX(CheckIn) as MostRecentCheckin,\n" +
+            "    MAX(Checkout) as MostRecentCheckout\n" +
+            "    from lab7_reservations\n" +
+            "    group by Room\n" +
+            "),\n" +
+            "FirstAvailables as (\n" +
+            "    select Room,MIN(CheckOut) as FirstAvailable from lab7_reservations r1" +
+            "    where CheckOut >= CURRENT_DATE" +
+            "    and not exists (select Room from lab7_reservations r2 where r1.Room = r2.Room" +
+            "    and r1.CheckOut = r2.CheckIn))" +
+            "select\n" +
+            "MostRecentReservation.Room,\n" +
+            "ROUND(DaysOccupied / 180, 2) as Popularity,\n" +
+            "DATE_ADD(MostRecentCheckout, interval 1 day) as FirstAvailable,\n" +
+            "DATEDIFF(MostRecentCheckout,MostRecentCheckin) as LastStayLength,\n" +
+            "MostRecentCheckout\n" +
+            "from MostRecentReservation\n" +
+            "join DaysOccupiedLast180 on DaysOccupiedLast180.Room = MostRecentReservation.Room\n" +
+            "join FirstAvailables on MostRecentReservation.Room = FirstAvailables.Room" +
+            ";";
+
+      try (Statement statement = connection.createStatement()) {
+           ResultSet rs = statement.executeQuery(sql);
+
+           String resultString = "";
+           while (rs.next()) {
+              String room = rs.getString("Room");
+              Float popularity = rs.getFloat("Popularity");
+              Date firstAvailable = rs.getDate("FirstAvailable");
+              int lastStayLength = rs.getInt("LastStayLength");
+              Date mostRecentCheckout = rs.getDate("MostRecentCheckout");
+              resultString += String.format("\nRoom: %s\nPopularity: %.2f\nFirst Available: %tF\nLast Stay Length: %d\nMost RecentCheckout: %tF\n",
+              room, popularity, firstAvailable, lastStayLength, mostRecentCheckout);
+           }
+           return resultString;
+      } catch (SQLException se) {
+         return "Failed to run query";
+      }
    }
 
    public String reservations() {
